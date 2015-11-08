27986556cdd7c211fc0a6be6b78796bdb401c6cb
Maciej Kurek <maciejk77@gmail.com>
2015-11-08 11:31:08 +0000

    Another test Travis CI should fail, missing semi-colon

--- hello.js
+++ hello.js
@@ -1,8 +1,8 @@
 var http = require('http');
-
-http.createServer(function(req, res) {
+ 
+http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}.listen(1337, '127.0.0.1')
-
-console.log('Server is running at http://127.0.0.1:1337/');
+  res.end('Hello World\n') // missing semi-colon will fail the build
+}).listen(1337, '127.0.0.1');
+ 
+console.log('Server running at http://127.0.0.1:1337/');
